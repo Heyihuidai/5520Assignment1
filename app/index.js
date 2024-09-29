5206a445e55c0501cc06dee7ecc13fec75fa1c8e
Shuojun Chen <shuojun@Shuojuns-MacBook-Air.local>
2024-09-29 15:13:38 -0700

    remove and add files to solve the display issue

---
+++ app/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+import StartScreen from '../screens/StartScreen';
+
+export default function App() {
+  const handleRegister = (info) => {
+    console.log('User registered:', info);
+  };
+
+  return (
+    <View style={styles.container}>
+      <StartScreen onRegister={handleRegister} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
