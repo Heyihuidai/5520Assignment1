a8a9b16e7ed9e5648ec491a994d47250c72dc77c
Shuojun Chen <shuojun@Shuojuns-MacBook-Air.local>
2024-09-29 13:27:08 -0700

    add validateName function

--- screens/StartScreen.js
+++ screens/StartScreen.js
@@ -11,3 +11,15 @@ const StartScreen = ({ onRegister }) => {
     const [phone, setPhone] = useState('');
     const [isChecked, setIsChecked] = useState(false);
     const [errors, setErrors] = useState({});
+
+    const validateName = (text) => {
+        if (text.length <= 1 || /\d/.test(text)) {
+          setErrors(prev => ({ ...prev, name: 'Invalid name' }));
+        } else {
+          setErrors(prev => ({ ...prev, name: null }));
+        }
+        setName(text);
+      };
+
+
+}
